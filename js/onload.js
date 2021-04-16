374f935286cbec53d7f192be67bd17b3d70fb65b
Felipe Lopes <felipelm07@gmail.com>
2021-04-16 13:28:29 -0300

    ajuste

--- js/onload.js
+++ js/onload.js
@@ -14,6 +14,8 @@ window.onload = function () {
             read: 'true',
             write: 'true'
         },
+        key: 'e79af8dd8bf538b09bcba42ce87e18b6a6f81d5f3d108eea7396f88bd6a468aa',
+        response_type: 'token',
         expiration: 'never',
         success: authenticationSuccess,
         error: authenticationFailure
